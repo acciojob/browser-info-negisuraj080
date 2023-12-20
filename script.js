ea550df05bd140e8a96240ffd58d3b37fea64341
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-12-20 13:50:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let browser = navigator.appName;
+let version = navigator.appName;
+let browserInfo =  "You are using " + browser + " version " + version.
+
+	let browserInfoDiv = document.getElementById("browser-info");
+    browserInfoDiv.textContent = browserInfo;
